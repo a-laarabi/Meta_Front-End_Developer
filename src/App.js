155b053b35b2ef7738341c413209b55d7de3061e
Anasse LAARABI <contact.laarabi@gmail.com>
2023-02-13 05:48:05 +0100

    Create App && cursor_position

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CursorRenderProps from './components/cursor_position_with_render_props';
+
+function App() {
+  return (
+    <div className="App">
+      <CursorRenderProps />
+    </div>
+  );
+}
+
+export default App;
