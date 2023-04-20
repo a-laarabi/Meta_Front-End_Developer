ad397cea4405242705c0144694ae1f5a012ef966
Anasse LAARABI <contact.laarabi@gmail.com>
2023-04-20 22:17:04 +0000

    Update LandingSection.js

--- Advanced React/src/components/Card.js
+++ Advanced React/src/components/Card.js
@@ -4,9 +4,6 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
 const Card = ({ title, description, imageSrc }) => {
-  // Implement the UI for the Card component according to the instructions.
-  // You should be able to implement the component with the elements imported above.
-  // Feel free to import other UI components from Chakra UI if you wish to.
   return (
     <VStack spacing={4} align="start" backgroundColor="white" borderRadius="2xl" color="black">
       <Image src={imageSrc} borderRadius="2xl" />
--- Advanced React/src/components/LandingSection.js
+++ Advanced React/src/components/LandingSection.js
@@ -6,8 +6,6 @@ const greeting = "Hello, I am Pete!";
 const bio1 = "I am a frontend developer";
 const bio2 = "and React Expert";
 
-// Implement the UI for the LandingSection component according to the instructions.
-// Use a combination of Avatar, Heading and VStack components.
 const LandingSection = () => (
   <FullScreenSection
     justifyContent="center"
@@ -16,11 +14,15 @@ const LandingSection = () => (
     backgroundColor="#2A4365"
   >
     <VStack spacing={4}>
-      <Avatar size="2xl"  src="https://i.pravatar.cc/150?img=7" />
-      <Heading size="md">{greeting}</Heading>
+      <Avatar
+        size="2xl"
+        src="https://i.pravatar.cc/150?img=7"
+        name="Peter Avatar"
+      />
+      <Heading as="h4" size="md">{greeting}</Heading>
       <VStack height={4} />
-      <Heading>{bio1}</Heading>
-      <Heading>{bio2}</Heading>
+      <Heading as="h1" size="3xl" noOfLines={1}>{bio1}</Heading>
+      <Heading as="h1" size="3xl" noOfLines={1}>{bio2}</Heading>
     </VStack>
   </FullScreenSection>
 );
