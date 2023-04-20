cf309ff0f3442b5700271e4d1425b375ef2b97db
Anasse LAARABI <contact.laarabi@gmail.com>
2023-04-20 22:17:40 +0000

    Update ContactMeSection.js

--- Advanced React/src/components/ContactMeSection.js
+++ Advanced React/src/components/ContactMeSection.js
@@ -1,5 +1,5 @@
 import React, {useEffect} from "react";
-import { useFormik, yupToFormErrors } from "formik";
+import { useFormik } from "formik";
 import {
   Box,
   Button,
@@ -17,102 +17,100 @@ import FullScreenSection from "./FullScreenSection";
 import useSubmit from "../hooks/useSubmit";
 import {useAlertContext} from "../context/alertContext";
 
-const LandingSection = () => {
-  const {isLoading, response, submit} = useSubmit();
-  const { onOpen } = useAlertContext();
+const ContactMeSection = () => {
+ const {isLoading, response, submit} = useSubmit();
+ const { onOpen } = useAlertContext();
 
-  useEffect(() => {
-    if (response) {
-      onOpen(response.type, response.message);
-      if (response.type === 'success') {
-        formik.resetForm();
-      }
-    }
-  }, [response]);
+ const formik = useFormik({
+   initialValues: {
+     firstName: "",
+     email: "",
+     type: "hireMe",
+     comment: "",
+   },
+   onSubmit: (values) => {
+     submit('https://john.com/contactme', values);
+   },
+   validationSchema: Yup.object({
+     firstName: Yup.string().required("Required"),
+     email: Yup.string().email("Invalid email address").required("Required"),
+     comment: Yup.string()
+       .min(25, "Must be at least 25 characters")
+       .required("Required"),
+   }),
+ });
 
-  const formik = useFormik({
-    initialValues: {
-      firstName: '',
-      email: '',
-      type: '',
-      comment: ''
-    },
-    onSubmit: (values) => {
-      submit("/#contact-me", values)
-    },
-    validationSchema: Yup.object({
-      firstName: Yup.string().required(),
-      email: Yup.string().required().email(),
-      type: Yup.string().oneOf(["hireMe", "openSource", "other"]),
-      comment: Yup.string().required()
-    }),
-  });
+ useEffect(() => {
+   if (response) {
+     onOpen(response.type, response.message);
+     if (response.type === 'success') {
+       formik.resetForm();
+     }
+   }
+ }, [response]);
 
-  return (
-    <FullScreenSection
-      isDarkBackground
-      backgroundColor="#512DA8"
-      py={16}
-      spacing={8}
-    >
-      <VStack w="1024px" p={32} alignItems="flex-start">
-        <Heading as="h1" id="contactme-section">
-          Contact me
-        </Heading>
-        <Box p={6} rounded="md" w="100%">
-          <form onSubmit={(e) => {
-            e.preventDefault();
-            formik.handleSubmit(e)
-            }}>
-            <VStack spacing={4}>
-              <FormControl isInvalid={formik.touched.firstName && !!formik.errors.firstName}>
-                <FormLabel htmlFor="firstName">Name</FormLabel>
-                <Input
-                  id="firstName"
-                  name="firstName"
-                  {...formik.getFieldProps("firstName")}
-                />
-                <FormErrorMessage>Required</FormErrorMessage>
-              </FormControl>
-              <FormControl isInvalid={formik.touched.email && !!formik.errors.email}>
-                <FormLabel htmlFor="email">Email Address</FormLabel>
-                <Input
-                  id="email"
-                  name="email"
-                  type="email"
-                  {...formik.getFieldProps("email")}
-                />
-                <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
-              </FormControl>
-              <FormControl>
-                <FormLabel htmlFor="type">Type of enquiry</FormLabel>
-                <Select id="type" name="type" {...formik.getFieldProps("type")}>
-                  <option value="hireMe">Freelance project proposal</option>
-                  <option value="openSource">
-                    Open source consultancy session
-                  </option>
-                  <option value="other">Other</option>
-                </Select>
-              </FormControl>
-              <FormControl isInvalid={formik.touched.comment && !!formik.errors.comment}>
-                <FormLabel htmlFor="comment">Your message</FormLabel>
-                <Textarea
-                  id="comment"
-                  name="comment"
-                  height={250}
-                  {...formik.getFieldProps("comment")}
-                />
-                <FormErrorMessage>Required</FormErrorMessage>
-              </FormControl>
-              <Button type="submit" colorScheme="purple" width="full" isLoading={isLoading}>
-                Submit
-              </Button>
-            </VStack>
-          </form>
-        </Box>
-      </VStack>
-    </FullScreenSection>
-  );
+ return (
+   <FullScreenSection
+     isDarkBackground
+     backgroundColor="#512DA8"
+     py={16}
+     spacing={8}
+   >
+     <VStack w="1024px" p={32} alignItems="flex-start">
+       <Heading as="h1" id="contactme-section">
+         Contact me
+       </Heading>
+       <Box p={6} rounded="md" w="100%">
+         <form onSubmit={formik.handleSubmit}>
+           <VStack spacing={4}>
+             <FormControl isInvalid={!!formik.errors.firstName && formik.touched.firstName}>
+               <FormLabel htmlFor="firstName">Name</FormLabel>
+               <Input
+                 id="firstName"
+                 name="firstName"
+                 {...formik.getFieldProps("firstName")}
+               />
+               <FormErrorMessage>{formik.errors.firstName}</FormErrorMessage>
+             </FormControl>
+             <FormControl isInvalid={!!formik.errors.email && formik.touched.email}>
+               <FormLabel htmlFor="email">Email Address</FormLabel>
+               <Input
+                 id="email"
+                 name="email"
+                 type="email"
+                 {...formik.getFieldProps("email")}
+               />
+               <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+             </FormControl>
+             <FormControl>
+               <FormLabel htmlFor="type">Type of enquiry</FormLabel>
+               <Select id="type" name="type" {...formik.getFieldProps("type")}>
+                 <option value="hireMe">Freelance project proposal</option>
+                 <option value="openSource">
+                   Open source consultancy session
+                 </option>
+                 <option value="other">Other</option>
+               </Select>
+             </FormControl>
+             <FormControl isInvalid={!!formik.errors.comment && formik.touched.comment}>
+               <FormLabel htmlFor="comment">Your message</FormLabel>
+               <Textarea
+                 id="comment"
+                 name="comment"
+                 height={250}
+                 {...formik.getFieldProps("comment")}
+               />
+               <FormErrorMessage>{formik.errors.comment}</FormErrorMessage>
+             </FormControl>
+             <Button type="submit" colorScheme="purple" width="full" isLoading={isLoading}>
+               Submit
+             </Button>
+           </VStack>
+         </form>
+       </Box>
+     </VStack>
+   </FullScreenSection>
+ );
 };
 
-export default LandingSection;
+export default ContactMeSection;
